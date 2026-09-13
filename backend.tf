ffcfc1fc996cc60c07399903f7bed82dc5d1283f
apps <23049337+yevgenis-shapiro-max2006@users.noreply.github.com>
2026-09-13 22:23:58 +0300

    update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "apps-terraform"
-    key    = "eks-migration/terraform.tfstate"
-    region = "eu-west-1"
+    key    = "eks-migration-workflow/terraform.tfstate"
+    region = "eu-central-1"
   }
 }
